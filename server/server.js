6d1fdd7deefd9705282395e9137093028570865c
Betnexa Developer <development@betnexa.com>
2026-02-23 10:16:11 +0300

    Update CORS to allow Vercel backend self-calls and frontend access

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json());s://server-chi-orcin.vercel.app', 'http
 app.use(express.urlencoded({ extended: true }));
 
 // Request logging
